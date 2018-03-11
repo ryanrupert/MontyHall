030840d8606d94f6e79a1250c4ed1954059e54c7
Ryan Rupert <mudge25+code@gmail.com>
2018-03-10 23:05:30 -0800

    Modify source.cpp
    This adds the better random number generator.

--- source.cpp
+++ source.cpp
@@ -10,3 +10,14 @@
 #include <chrono>
 #include <random>
 #include <time.h>
+
+uint32_t seedseq_random_using_clock()
+{
+    uint64_t seed = std::chrono::high_resolution_clock::
+                                        now().time_since_epoch().count();
+    std::seed_seq seeder{uint32_t(seed),uint32_t(seed >> 32)};
+    ++seed;
+    int out;
+    seeder.generate(&out,&out+1);
+    return out;
+}
